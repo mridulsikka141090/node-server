3cdd02d445c1a151edea8de3d27ec3e1475971a0
Mridul Sikka <msikka@invidi.se>
2020-03-02 00:08:32 +0100

    added new server file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 8000;
+
+//cors policy
+app.use(cors());
+
+// parse body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+
+//Listen on port
+app.listen(PORT,() => {
+    console.log(`The server is listening on Port: ${PORT}`);
+}) 
